4e5a1b04b01a510b8bbc3364e1326cb7ae9bec6a
ShanLi <ShanLi@9.200.36.147>
2018-05-26 16:09:43 +0800

    test

---
+++ src/com/leo/oom/HeapOOM.java
@@ -0,0 +1,19 @@
+package com.leo.oom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapOOM {
+
+    int a = 1;
+    public static void main(String[] args) {
+
+        HeapOOM h = new HeapOOM();
+        h.testSoom();
+    }
+
+    public void testSoom() {
+        int b = 4;
+        int c = 6;
+    }
+}
